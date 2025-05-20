38929b90ae75047c1bcc212bb4c63d4e665e5f04
41KailasRakh <kailasrakh@gmail.com>
2025-05-20 20:23:14 +0530

    Added some basic Integration and Unit Test cases

---
+++ order-service/src/test/java/com/kailaslabs/bookstore/orders/controllers/OrdersControllerTests.java
@@ -0,0 +1,67 @@
+package com.kailaslabs.bookstore.orders.controllers;
+
+import com.kailaslabs.bookstore.orders.AbstractIT;
+import com.kailaslabs.bookstore.orders.testdata.TestDataFactory;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@Sql("/test-orders.sql")
+class OrdersControllerTests extends AbstractIT {
+
+    @Nested
+    class CreateOrderTests {
+        @Test
+        void shouldCreateOrderSuccessfully() {
+            var payload =
+                    """
+                                {
+                                    "customer" : {
+                                        "name": "Siva",
+                                        "email": "siva@gmail.com",
+                                        "phone": "999999999"
+                                    },
+                                    "deliveryAddress" : {
+                                        "addressLine1": "HNO 123",
+                                        "addressLine2": "Kukatpally",
+                                        "city": "Hyderabad",
+                                        "state": "Telangana",
+                                        "zipCode": "500072",
+                                        "country": "India"
+                                    },
+                                    "items": [
+                                        {
+                                            "code": "P100",
+                                            "name": "Product 1",
+                                            "price": 25.50,
+                                            "quantity": 1
+                                        }
+                                    ]
+                                }
+                            """;
+            given().contentType(ContentType.JSON)
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("orderNumber", notNullValue());
+        }
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenMandatoryFieldsMissing() {
+        var payload = TestDataFactory.createOrderRequestWithInvalidCustomer();
+        given().contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+}
